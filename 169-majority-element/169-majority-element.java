f3984a4c3f3435c0a572a3f4501be93cb10039f6
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-06-11 03:52:50 +0530

    Time: 3 ms (59.65%), Space: 56.9 MB (14.78%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,12 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-    int n= (int)Math.floor(nums.length/2);
-        Arrays.sort(nums);
-        return nums[n];
+   //since the element is majority, when we update count it will neve be 0 or lesser
+        // because the el comes n/2 times and more.
+        // so in no way the count will be 0 for case of majority element till the very last
+        // even if the majority element is alternately arranged, its count is more than n/2
+        // sp count value will not be 0 for it till the end
+        
+        int count=1, majority=nums[0];
+        for(int i=1;i<nums.length;i++){
+            
+            if(count==0){
+                majority=nums[i];
+            }
+            
+            if(nums[i]!= majority){
+                count--;
+            }
+            else{
+                count++;
+            }
+            
+        }
      
-    
-    
+        
+        return majority;
     
     }
 }
