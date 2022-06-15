0ed970070b373a86f7263300bac54ce16c63c8b0
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-06-16 01:43:31 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (37.47%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,23 +11,30 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        //if a node is visited already, then return true
+        //floyd detection
         
-        //if its a cycle it will never terminate on its own
-        // so i put a terminating condition 
-        //if its not a cycle then it will automatically terminate when loop condn fails
-        ListNode l1= head;
-        HashMap<ListNode, Integer> h1= new HashMap<>();
-        while(l1!=null){
+        
+        // keep iterating slowly one pointer for each node,
+        // mark it as one time visited
+        // keep fast moving 2 steps ahead
+        // if fast comes back to same position as slow somehow,
+        // there must be a cycle in ll
+        
+        ListNode slow=head, fast=head;
+        if(fast==null){
+            return false;
+        }
+        while(fast!=null && fast.next!=null){
             
-            if(h1.containsKey(l1)){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            h1.put(l1,0);
-            l1=l1.next;
         }
         return false;
         
-    }     
-   
+        // if null is slow means no such cycle
+    
+    }
 }
