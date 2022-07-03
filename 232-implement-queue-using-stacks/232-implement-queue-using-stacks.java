5019a5cf1784d52665cac789d850977ba76d7ea9
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-03 22:52:01 +0530

    Time: 1 ms (67.24%), Space: 42.3 MB (15.83%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -1,51 +1,46 @@
 class MyQueue {
+//use 2 stacks
+    //fill the stack pop when required
+    //until then keep filling in ele in the stack push
+    Stack<Integer> s1=new Stack<>();
+    Stack<Integer> s2=new Stack<>();
 
-    //use stack to implement a queue
-    Stack<Integer> s1;
-    Stack<Integer> s2;
     public MyQueue() {
-       s1 = new Stack<>();
-        s2=new Stack<>();
+        
     }
     
     public void push(int x) {
-        //empty from s1 to s2
-        //put new element into s1
-        //o maintain lilo order
-        //.push back from s2 tos1
         
-        if(s1.empty()){
-            s1.push(x);
-            return;
-        }
-        
-        while(!s1.empty()){
-            s2.push(s1.pop());
-        }
         s1.push(x);
-        while(!s2.empty()){
-            s1.push(s2.pop());
-        }
         
     }
     
     public int pop() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.pop());
+            }
+        }
         
-        if(s1.empty())
-            return -1;
-        return s1.pop();
+        return s2.pop();
         
     }
     
     public int peek() {
-        if(s1.empty())
-            return -1;
-        return s1.peek();
+        
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.pop());
+            }
+            
+        }
+        return s2.peek();
         
     }
     
     public boolean empty() {
-        return s1.empty();
+       
+        return s1.empty()&&s2.empty();
         
     }
 }
