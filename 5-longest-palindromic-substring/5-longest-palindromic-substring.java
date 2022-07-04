5d1bbf18aa4c3ded7e86cc29c6c64d75a738500e
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-04 17:05:19 +0530

    Time: 74 ms (45.13%), Space: 54.6 MB (33.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,88 +1,67 @@
 class Solution {
-   
     public String longestPalindrome(String s) {
+       
+        //for each alphabet
+        //find the string wich is the maximum palindrome for that part
+        //and compare its length with our currnt max
+        //if its more thn current max then it is the new max string
         
-         int[][] dp=new int[s.length()][s.length()];
+        //use 2 pointers and check for even and odd length
+        // with even length the problem is the string will only expand oddly and not check the even cases of the substring
+        //hence we use both of them and se which one gives a better result
         
-        for(int i=0;i<dp.length;i++){
-            for(int j=dp.length-1;j>=0;j--){
-                if(i<=j){
-                    if(i==j){
-                    dp[i][j]=1;
-                    continue;
-                    // single length substring is a paindrome always
-                    
-                }
-                
-                if(j-i==1){
-                    // 2 length substring
-                    if(s.charAt(i)==s.charAt(j)){
-                        dp[i][j]=1;
-                    }
-                    else{
-                         dp[i][j]=0;
-                    }
-                }
-                
-                // BAse Cases
-                 
-                }
-                
-                
-                
-                
-            }
+        
+        
+        String s1="";
+        if(s.length()==2){
+            if(s.charAt(0)==s.charAt(1))
+                return s;
+            else
+                return s1+s.charAt(0);
         }
         
-        int maxlen=0;
-        String S="";
-        String res="";
-        // start from last row as we require results from prev filled rows
-        for(int i=dp.length-1;i>=0;i--){
-            for(int j=i;j<dp.length;j++){
-                 int left=i;
-                int right=j;
-               
-                    if(i==j){
-                   S=s.substring(left,right+1);
-                    if(S.length()>res.length()){
-                        res=S;
-                    }
-                        continue;
-                        
-                    
-                    
-                }
-                
-                if(j-i==1 && dp[i][j]==1){
-                    S=s.substring(left,right+1);
-                    if(S.length()>res.length()){
-                        res=S;
-                    }
-                    continue;
-                }
-                else if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==1){
-                    dp[i][j]=1;
-                    S=s.substring(left,right+1);
-                    if(S.length()>res.length()){
-                        res=S;
-                    }
-
-                }
-                else{
-                    dp[i][j]=0;
-                }
-               
-            }
-        }
-        // for(int i=0;i<dp.length;i++){
-        //     for(int j=i;j< dp.length;j++){
-        //        System.out.print(dp[i][j]+" ") ;
-        //     }
+        //this i now the case for 1 length atleast substring, since we know edge elements are anyway useless for us
         
-            //    System.out.println();
-            // }
-        return res;
+        //as with last char, we only get amax of 1 since we can't compare it with its next element
+        
+        s1=s1+s.charAt(0);
+        int maxlen=1;
+        for(int i=0;i<s.length()-1;i++){
+            
+            String a=findpalindrome(i,i, s);
+            
+            String b= "";
+            
+            if(s.charAt(i)==s.charAt(i+1))
+            b=findpalindrome(i,i+1,s);
+            
+            if(a.length()>maxlen){
+                s1=a;
+                maxlen=a.length();
+            }
+            
+           if(b.length()>maxlen){
+               s1=b;
+               maxlen=b.length();
+           }
+            
+        }
+        
+        
+        return s1;
+        
+    }
+    
+    String findpalindrome(int i, int j, String s){
+        int start=i,end=j;
+        while(i>=0 && j< s.length() && s.charAt(i)==s.charAt(j)){
+            start=i;
+            end=j;
+            i--;
+            j++;
+        }
+        
+        return s.substring(start,end+1);
         
     }
 }
