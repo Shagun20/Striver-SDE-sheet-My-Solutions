584a88243173b0d2fc612d6b1585903a9501cbba
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-07 16:22:26 +0530

    Time: 0 ms (100.00%), Space: 42 MB (70.28%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -16,20 +16,52 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
-        List<Integer> ans=new ArrayList<>();
-        inorder(root, ans);
+         List<Integer> ans= new ArrayList<>();
+    TreeNode current=root;
+        while(current!=null){
+            
+            if(current.left==null){
+                //add to ans
+                ans.add(current.val);
+                //move to right
+                current=current.right;
+                
+            }
+            else{
+                
+                //we will first see and create  link to current node
+                //from rightmost child
+                
+                //so we will create a link
+                
+                TreeNode ptr=current.left;
+                //ptr can't be null
+                
+                //see if rightmost in left half already does not have a pointer
+                while(ptr.right!=null && ptr.right!=current){
+                    ptr=ptr.right;
+                }
+                
+                //if no link exists aready then create one
+                if(ptr.right==null){
+                     ptr.right=current;
+                     current=current.left;
+                    
+                }
+                  
+                else if(ptr.right==current){
+                    //break the link
+                    ptr.right=null;
+                    ans.add(current.val);
+                    //move current ahead rightwards
+                    current=current.right;
+                }
+                
+                
+                
+            }
+            
+        }
         return ans;
-      
-        
-    }
-    public void inorder(TreeNode root, List<Integer> ans){
-        //fill in the list by reference only
-        if(root==null)
-            return;
-        inorder(root.left, ans);
-        ans.add(root.val);
-        inorder(root.right, ans);
-        
-        
     }
 }
