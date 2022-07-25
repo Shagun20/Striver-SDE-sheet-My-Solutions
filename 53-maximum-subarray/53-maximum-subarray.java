15706871a1d5122280c8b40821983f0fd26254e0
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-25 17:22:10 +0530

    Time: 2 ms (71.29%), Space: 81.6 MB (14.21%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,32 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         
-        //kadane's algo
+        int curr=0;
+        int max=-10000;
         
-        int cur_max=nums[0];
-        int globalmax=cur_max;
-        
-        for(int i=1;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
+            curr=curr+nums[i];
             
-             if(cur_max>=globalmax){
-                globalmax=cur_max;
+            if(nums[i]>curr){
+                curr=nums[i];
             }
-            
-            if(nums[i]+cur_max > nums[i]){
-                cur_max=cur_max+nums[i];
-                
-            }
-            else{
-                cur_max=nums[i];
-            }
-            
-           
+            max=Math.max(curr,max);
         }
         
-        if(cur_max>=globalmax){
-                globalmax=cur_max;
-            }
-        
-      return globalmax;
+        return max;
     }
 }
