4b3e8449b540000bd096af535c1d919b193fc6f8
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-26 01:09:48 +0530

    Time: 1 ms (90.40%), Space: 48.2 MB (39.37%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        //recursion 
+                if (image[sr][sc] == color) return image;
+
+        
+      return color(image, sr, sc, image[sr][sc], color);
+        
+        
+        
+    }
+    
+    public int[][] color(int[][] image, int sr, int sc, int precol, int color){
+        
+        
+        int rows=image.length;
+        int col=image[0].length;
+       // 
+        if(sr<0 || sc<0 || sr>=rows || sc>=col){
+           // System.out.println(rows+" "+col);
+            return image;
+        }
+        
+        if(image[sr][sc]==precol){
+            
+        image[sr][sc]=color;
+        image= color(image, sr+1, sc, precol, color);
+        image= color(image, sr, sc+1, precol, color);
+        image= color(image, sr-1, sc,precol, color);
+        image= color(image, sr, sc-1, precol, color);
+        }
+            
+        
+        return image;
+        
+        
+    }
+}
