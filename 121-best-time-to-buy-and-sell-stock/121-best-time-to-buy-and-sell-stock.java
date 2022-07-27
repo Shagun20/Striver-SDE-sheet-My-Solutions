584dd197a5883ef66c9700577cb304673059604e
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-27 18:04:08 +0530

    Time: 2 ms (93.25%), Space: 59 MB (90.24%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,36 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // use 2 pointers 
-        
-        
-        // upgrade the right pointer till either right pointer value >= left
-        //or right reaches its maxposition
-        // upgrade left pointer to right only if its right value is smaller than its current value,
-        // check for right and keep updating left and right till they make a valid pair
-        // 
-        
-        int globalmax=0;
-        int curleft=0;
-        int curright=1;
         
         if(prices.length==1){
             return 0;
+        }
+        int max= prices[prices.length-1];
+        int profit=0;
+        for(int i=prices.length-2;i>=0;i--){
+            
+            int p=max-prices[i];
+            
+            profit=Math.max(profit,p);
+            
+            max= Math.max(max, prices[i]);
             
         }
         
-        for(int i=curright;i<prices.length;i++){
-            if(prices[i]-prices[curleft]>0){
-                if(prices[i]-prices[curleft]>globalmax){
-                    globalmax=prices[i]-prices[curleft];
-                }
-            }
-            else{
-                curleft=i;
-            }
-        }
-        
-        
-        
-        return globalmax;
+        return profit;
     }
 }
