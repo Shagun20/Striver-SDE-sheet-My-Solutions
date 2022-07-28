f3b3d41d6b5fc81e858b843f14f72d57c4d568d2
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-07-28 16:45:17 +0530

    Time: 1 ms (72.34%), Space: 41.6 MB (71.55%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,47 +1,38 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new  ArrayList<List<Integer>>();
-        List<Integer> c1= new ArrayList<Integer>();
-        c1.add(1);
-        ans.add(c1);
         
-        for(int i=1; i<numRows;i++){
+        List<List<Integer>> ans= new ArrayList<>();
+        
+        
+        List<Integer> a1= new ArrayList<>();
+        
+         List<Integer> a2 = new ArrayList<>();
+        a1.add(1);
+        ans.add(a1);
+        
+        while(--numRows>0){
             
-            //a new list for each row
+           a1=ans.get(ans.size()-1);
+            a2= new ArrayList<>();
+            int left=0;
+            int right=0;
             
-            c1= new ArrayList<Integer>();
-            List<Integer> refer = ans.get(i-1);
-            
-            
-            int left=-1; int top=0;
-            
-            
-            while(left!=refer.size()){
+            while(right!=a1.size()){
                 
-                int ans1;
+                int sum= left+ a1.get(right);
                 
-                int ans2;
-                if(left==-1){
-                    ans1=0;
-                }
-                else{
-                    ans1=refer.get(left);
-                }
-                if(top==refer.size()){
-                    ans2=0;
-                }
-                else{
-                    ans2= refer.get(top);
-                }
-                
-                c1.add(ans1+ans2);
-                left++;
-                top++;
-                
-                
+                a2.add(sum);
+                left=a1.get(right);
+                right=right+1;
             }
-            ans.add(c1);
+            
+            
+            a2.add(left+0);
+            
+            ans.add(a2);
+            
         }
         return ans;
+        
     }
 }
