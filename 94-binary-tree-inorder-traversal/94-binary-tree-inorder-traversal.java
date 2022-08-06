a761b71ae70ae8063eccc85a4f41f02e8ec47d86
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-08-06 20:48:42 +0530

    Time: 1 ms (50.41%), Space: 43 MB (5.42%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,53 +15,19 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+      
+         if(root==null){
+           List<Integer> ans= new ArrayList<>();
+           return ans;
+         }
+      
+      List<Integer> a1= inorderTraversal(root.left);
+      a1.add(root.val);
+      List<Integer> a2= inorderTraversal(root.right);
+      a1.addAll(a2);
+      
+      return a1;
+      
         
-         List<Integer> ans= new ArrayList<>();
-    TreeNode current=root;
-        while(current!=null){
-            
-            if(current.left==null){
-                //add to ans
-                ans.add(current.val);
-                //move to right
-                current=current.right;
-                
-            }
-            else{
-                
-                //we will first see and create  link to current node
-                //from rightmost child
-                
-                //so we will create a link
-                
-                TreeNode ptr=current.left;
-                //ptr can't be null
-                
-                //see if rightmost in left half already does not have a pointer
-                while(ptr.right!=null && ptr.right!=current){
-                    ptr=ptr.right;
-                }
-                
-                //if no link exists aready then create one
-                if(ptr.right==null){
-                     ptr.right=current;
-                     current=current.left;
-                    
-                }
-                  
-                else if(ptr.right==current){
-                    //break the link
-                    ptr.right=null;
-                    ans.add(current.val);
-                    //move current ahead rightwards
-                    current=current.right;
-                }
-                
-                
-                
-            }
-            
-        }
-        return ans;
     }
 }
