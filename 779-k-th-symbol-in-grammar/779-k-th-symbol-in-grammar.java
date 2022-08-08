cf279aaffa04ddba8a5067c183a0dc2dbc48068e
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-08-08 19:46:58 +0530

    Time: 34 ms (11.26%), Space: 42 MB (6.43%) - LeetHub

---
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int kthGrammar(int n, int k) {
+      
+       
+      
+      if(n==1 || k==1){
+        return 0;
+      }
+      
+      
+      int p= (int)Math.pow(2,n-2);
+      System.out.println(n+" "+k+" "+p);
+      
+      if(k<=p){
+       
+        return kthGrammar(n-1,k);
+      }
+      
+      
+      else{
+        return (1+ kthGrammar(n, k-p))%2;
+      }
+        
+    }
+}
