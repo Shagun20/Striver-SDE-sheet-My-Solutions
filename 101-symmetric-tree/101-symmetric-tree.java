ddc5b4ee585aaa5f85779a0f31fb847bf9a6a517
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-08-09 14:18:25 +0530

    Time: 1 ms (64.72%), Space: 42.8 MB (19.71%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,25 +15,28 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+      
+      return check(root.left, root.right);
         
-        //check if root.left and root.right are identical trees
-        
-        return identical(root.left, root.right);
-        
-        
+    }
+  
+  
+  public boolean check(TreeNode root1, TreeNode root2){
+    
+    if(root1==null && root2==null){
+      return true;
     }
     
-    public boolean identical(TreeNode root1, TreeNode root2){
-        
-        if(root1==null && root2==null)
-            return true;
-        
-        if(root2==null|| root1==null)
-            return false;
-        
-        if(root1.val!=root2.val)
-            return false;
-        
-        return identical(root1.left,root2.right) && identical(root1.right, root2.left);
+    else if(root1==null || root2==null){
+      return false;
     }
+    
+    if(root1.val==root2.val){
+     return check(root1.left, root2.right) && check(root1.right, root2.left);
+    }
+    
+    else{
+      return false;
+    }
+  }
 }
