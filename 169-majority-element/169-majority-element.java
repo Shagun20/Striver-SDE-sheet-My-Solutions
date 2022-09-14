caacd67ac42f39db9dc49fd88328a005f07059a9
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-09-15 00:23:11 +0530

    Time: 272 ms (5.11%), Space: 50.1 MB (85.31%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,30 +1,34 @@
 class Solution {
+  
+  //moore's voting algorithm
+  
+  
     public int majorityElement(int[] nums) {
+      
+      int count=0;
+      
+      int cand=nums[0];
+      
+      for(int ele:nums){
+        System.out.println(cand+" "+count+" "+ele);
         
-   //since the element is majority, when we update count it will neve be 0 or lesser
-        // because the el comes n/2 times and more.
-        // so in no way the count will be 0 for case of majority element till the very last
-        // even if the majority element is alternately arranged, its count is more than n/2
-        // sp count value will not be 0 for it till the end
-        
-        int count=1, majority=nums[0];
-        for(int i=1;i<nums.length;i++){
-            
-            if(count==0){
-                majority=nums[i];
-            }
-            
-            if(nums[i]!= majority){
-                count--;
-            }
-            else{
-                count++;
-            }
-            
+        if(ele==cand){
+          count++;
         }
-     
         
-        return majority;
-    
+        else{
+          count--;
+          
+          if(count==0){
+            cand= ele;
+            count=1;
+          }
+        }
+      }
+        System.out.println(cand+" "+count);
+      
+      return cand;
+      
+        
     }
 }
