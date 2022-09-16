3f7324105962066b64b8baf9d0a7d097bbadd8b7
Shagun20 <85337809+Shagun20@users.noreply.github.com>
2022-09-16 12:53:26 +0530

    Time: 102 ms (5.03%), Space: 55.2 MB (59.91%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,61 +1,65 @@
-import java.util.*;
 class Solution {
+  
+   public class arraycompare implements Comparator<int[]>{
+     
+     @Override
+     public int compare(int[] obj1, int[] obj2){
+       
+       int start1= obj1[0];
+       int start2= obj2[0];
+       
+       return(start1- start2);
+       
+     }
+     
+   }
     public int[][] merge(int[][] intervals) {
         
-        int[] compare = new int[2];
-       
-        Arrays.sort(intervals, new Comparator<int[]>() {
-        @Override
-        public int compare(int[] arr1, int[] arr2) {
-            if(arr1[0] < arr2[0]) return -1;
-            if(arr1[0] == arr2[0]) return 0;
-               else return 1;
+      //sort based on starting points
+      //start from the point 1- point 2
+      Arrays.sort(intervals, new arraycompare());
+      for(int i=0;i<intervals.length;i++){
+        System.out.println(intervals[i][0]+" "+ intervals[i][1]);
+      }
+     
+      //create an arraylist of merged intervals
+      //convert arraylist to array
+      
+      ArrayList<int[]> ans= new ArrayList<>();
+      int left=0;
+      int right=left+1;
+      
+      if(intervals.length==1){
+        return intervals;
+      }
+      
+      ans.add(intervals[0]);
+      int anspointer= 0;
+      for(int ele=1; ele<intervals.length;ele++){
+        
+         int[] arr= intervals[ele];
+        
+        //compare
+        int start=arr[0];
+        int end= arr[1];
+        if(start<= ans.get(anspointer)[1]){
+          
+          //merge them
+          ans.get(anspointer)[1]= Math.max(end,  ans.get(anspointer)[1]);
+          
         }
-    });
-        
-        ArrayList<int[]> ans= new ArrayList<>();
-        ArrayList<int[]> ans2= new ArrayList<>();
-       for (int i=0;i<intervals.length;i++) {
-   
-              ans.add((intervals[i]));
-                                   
-              }
-        
-        
-        if(ans.size()==1){
-            return ans.toArray(new int[ans.size()][]);
+           
+        else{
+          ans.add(arr);
+          anspointer++;
         }
+      }
        
-        int j=1;
-        int n= ans.size();
-        compare[0]=ans.get(0)[0];
-        compare[1]=ans.get(0)[1];
-        
-        
-        while( j < ans.size()){
-          //  System.out.println(compare[0]+" "+compare[1]);
-            while( j < ans.size() && ans.get(j)[0]<= compare[1]){
-                
-                //overlap
-               
-                compare[0]= Math.min(ans.get(j)[0], compare[0]);
-                    compare[1]=Math.max(ans.get(j)[1], compare[1]);
-                j++;
-                
-                // iterate till they overlap and make compare as the new interval thats not overlapping
-            }
-            ans2.add(compare);
-            if(j<ans.size()){
-            compare=new int[2];
-            compare[0]= ans.get(j)[0];
-            compare[1]= ans.get(j)[1];
-            j++;
-            }
-       
-        }
-        if(!Arrays.equals(ans2.get(ans2.size()-1), compare) )
-            ans2.add(compare);
-        return ans2.toArray(new int[ans2.size()][]);
-        
+           
+      
+      
+  int[][] values= new int[ans.size()][2];
+      return ans.toArray(values);
+      
     }
 }
